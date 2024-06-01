a2983a5805fe96b6a3ccb2c8d5d69432858f595c
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-31 21:47:33 -0400

    Implement Employee class with name, salary properties, and annual salary calculation.

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+
+class Employee {
+    constructor(name, annualsalary) {
+        this.name = name;
+        this.annualsalary = annualsalary;
+    }
+
+    calculatemonthlysalary(){
+        calculate this.monthlysalary * 12;
+    }
+}
